290a22fa1af562bde21c12afbc78288f70197ff3
sony1011 <101799428+sony1011@users.noreply.github.com>
2023-07-28 13:37:40 +0530

    Create unzip_artifact.sh

---
+++ unzip_artifact.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Navigate to the location where the zip file is stored
+cp s3://codepipeline-us-west-2-598491907540/SamplePipelineDemo/*.zip /tmp
+cd /tmp
+
+# Unzip the artifact
+unzip *.zip -d /var/www/html
+
+# Ensure the extracted files have the appropriate permissions (adjust as needed)
+chown -R www-data:www-data /var/www/html
